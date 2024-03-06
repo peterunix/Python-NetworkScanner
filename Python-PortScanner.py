38a23d927b5ae9981ffce26c26e83eae1fd6ae22
p <p>
2024-03-05 16:46:57 -0800

    Got the Mac and Vendor to be added to a dictionary with the keys being online host IP addresses. The mac address lookups dont run in parallel

--- Python-PortScanner.py
+++ Python-PortScanner.py
@@ -5,8 +5,6 @@ import socket
 import getmac
 from mac_vendor_lookup import MacLookup
 
-subnet = ipaddress.ip_network('192.168.50.0/24')
-
 # Checks if a device is online and appends it to the onlineHosts list
 def ping_device(ip: str):
     ipStr = str(ip)
@@ -18,40 +16,47 @@ def ping_device(ip: str):
             pass
     except Exception:
         message = f"Error pinging {ipStr}"
-
         print(message)
 
 # Grabs the Mac Address from a IP address
 def get_mac_address(ip: str) -> str:
     print("Mac code here")
-    mac = getmac.get_mac_address(ip="192.168.50.1")
+    mac = getmac.get_mac_address(ip=ip)
     vendor = MacLookup().lookup(mac)
     onlineHosts[ip] = {'MacAddress': mac, 'Vendor': vendor}
     return mac, vendor
 
+def main(subnet):
+    # Ping all hosts on the network using python threads
+    network = ipaddress.ip_network(subnet, strict=False)
+    threads = []
+    # Getting all online hosts
+    print("Getting online hosts")
+    for ip in network.hosts():
+        ipStr = str(ip)
+        print(ipStr)
+        if len(threads) == 25:
+            for thread in threads:
+                thread.join()
+                threads.clear()
 
-# Ping all hosts on the network using python threads
-network = ipaddress.ip_network(subnet, strict=False)
-onlineHosts = {}
-threads = []
-for ip in network.hosts():
-    ipStr = str(ip)
-    if len(threads) == 16:
-        for thread in threads:
-            thread.join()
-            threads.clear()
+        thread = threading.Thread(target=ping_device, args=(ipStr,))
+        # thread = threading.Thread(target=get_mac_address, args=(ipStr,))
+        thread.start()
+        threads.append(thread)
 
-    thread = threading.Thread(target=ping_device, args=(ipStr,))
-    thread = threading.Thread(target=get_mac_address, args=(ipStr,))
-    thread.start()
-    threads.append(thread)
-
-# Wait for all the threads to clear
-for thread in threads:
-    thread.join()
+    # Wait for all the threads to clear
+    for thread in threads:
+        thread.join()
     threads.clear()
 
-for key, value in onlineHosts.items():
-    print(key, value)
-# onlineHosts = sorted(onlineHosts)
-# print(onlineHosts)
+    # Getting Device Mac Addresses
+    for key in onlineHosts.keys():
+        mac, vendor = get_mac_address(key)
+        onlineHosts[key] = {'MacAddress': mac, 'Vendor': vendor}
+
+subnet = ipaddress.ip_network('192.168.50.0/24')
+onlineHosts = {}
+main(subnet)
+
+print(onlineHosts.items())
